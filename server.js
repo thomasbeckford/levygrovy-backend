29443f4f6e64fc8b93b1f05b10012a4bb093e9ad
Thomas Beckford <tbeckford@jahnelgroup.com>
2019-12-19 15:24:01 -0300

    first commit

--- server.js
+++ server.js
@@ -1,48 +1,74 @@
-const { ApolloServer, gql } = require('apollo-server-fastify');
-const { typeDefs, resolvers } = require('./module');
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
-
 const fastify = require('fastify')({
-  logger: true,
-  ignoreTrailingSlash: true
-})
+  logger: false,ignoreTrailingSlash: true
+}) // Fastify app adding PINO logger
 
+const { ApolloServer, gql } = require('apollo-server-fastify');
 const { Client } = require('pg');
 const connectionString = 'postgres://postgres:postgres@localhost:5432/postgres';
-
-const client = new Client({
-    connectionString: connectionString
-});
+const client = new Client({connectionString: connectionString});
 
 client.connect();
-
-const PORT = process.env.PORT || 3000;
 
 fastify.get('/', (req, res) => {
   res.send({hello: 'world'})
 })
 
-fastify.get('/artists', (req,res) => {
-    client.query('SELECT * FROM Employee where id = $1', [1], function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send(result.rows);
-    });
-})
+typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+  # This "Book" type can be used in other type declarations.
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Character {
+    name: String
+    id: ID
+    episodes: [String]
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    books: [Book]
+    characters: [Character]
+    character(id: ID!): Character
+  }
+
+  type Mutation {
+    createBook(title: String!): Book
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => knex("book").select("*"),
+  },
+
+  Mutation:{
+    createBook: async(_,{title, author}) => {
+      const [book] = await knex("book")
+        .returning("*")
+        .insert({title});
+      return book;
+    }
+  }
+
+};
+
+
+const server = new ApolloServer({typeDefs,resolvers});
 
 const start = async () => {
   try{
-    await fastify.listen(PORT, '0.0.0.0');
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    fastify.register(server.createHandler());
+    fastify.log.info(`server listening on ${fastify.server.address()} 4000`)
+    await fastify.listen(4000, '0.0.0.0');
   }
   catch (err){
-    fastify.log.error(err);
+    fastifyf.log.error(err);
     process.exit(1);
   }
 }
